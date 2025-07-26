e0a0f0bb270a2b0154f7d199ae7fe4b43b0df34f
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-07-26 21:46:44 +0530

    Update amplify_config.js

--- amplify_config.js
+++ amplify_config.js
@@ -1,8 +1,15 @@
 window.amplifyConfig = {
   Auth: {
     region: 'us-east-1',
-    userPoolId: 'YOUR_USER_POOL_ID',
-    userPoolWebClientId: 'YOUR_CLIENT_ID',
-    authenticationFlowType: 'USER_PASSWORD_AUTH'
+    userPoolId: 'us-east-1_RMhlKuGHS',
+    userPoolWebClientId: '4g0bo9ac68vq4g817v1je4e5sf', // Replace with your actual client ID
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+    oauth: {
+      domain: 'https://us-east-1rmhlkughs.auth.us-east-1.amazoncognito.com', // Replace with your domain
+      scope: ['email', 'profile', 'openid'],
+      redirectSignIn: 'https://gowthusaidatta.github.io/codeSync/index.html',
+      redirectSignOut: 'https://gowthusaidatta.github.io/codeSync/index.html',
+      responseType: 'code'
+    }
   }
 };
