053a7021be768c5e4d39ca0d916862b74b492293
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-07-26 21:03:02 +0530

    Add files via upload

---
+++ amplify_config.js
@@ -0,0 +1,8 @@
+window.amplifyConfig = {
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'YOUR_USER_POOL_ID',
+    userPoolWebClientId: 'YOUR_CLIENT_ID',
+    authenticationFlowType: 'USER_PASSWORD_AUTH'
+  }
+};
