b952134e4fe4bc643d3807c920ef9053b3633ac7
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-08-01 19:29:10 +0530

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  // This is a placeholder. 
-  // The content script injects the UI automatically.
-  // You could add logic here to programmatically toggle the UI if needed.
-  console.log("Pandu AI icon clicked.");
-});
